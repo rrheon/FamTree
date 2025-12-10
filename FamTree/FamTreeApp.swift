a21e6a4e609d4422e1715c4182b321245874c156
rrheon <yongheon0806@gmail.com>
2025-12-10 19:12:03 +0900

    Initial Commit

---
+++ FamTree/FamTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamTreeApp.swift
+//  FamTree
+//
+//  Created by 최용헌 on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FamTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
